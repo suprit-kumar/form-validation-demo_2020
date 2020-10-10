34920dcdebc4f1aa85b491dd50c4a654bcfa08f5
Supritkumar <supritk16@gmail.com>
2020-10-10 15:55:39 +0530

    password validation added in javascript

--- script.js
+++ script.js
@@ -5,22 +5,64 @@ const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
 let isValid = false;
-
+let passwordsMatch = false;
 
 function validateForm(){
     // Constraint API
     isValid = form.checkValidity();
     // Style main message for an error
-    message.textContent = 'Please fill out all the fields';
-    message.style.color = 'red';
-    messageContainer.style.borderColor = 'red';
+    if(!isValid){
+        message.textContent = 'Please fill out all the fields';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        return false;
+    }
+    // check password match
+    if(password1El.value === password2El.value){
+        passwordsMatch = true;
+        password1El.style.borderColor = 'green';
+        password2El.style.borderColor = 'green';
+    }else{
+        passwordsMatch = false;
+        message.textContent = 'Make sure passwords match';
+        message.style.color = 'red';
+        messageContainer.style.borderColor = 'red';
+        password1El.style.borderColor = 'red';
+        password2El.style.borderColor = 'red';
+        return false;
+    }
+
+    // if form is valid and password match
+    if(isValid && passwordsMatch){
+        message.textContent = 'Great! Successfully Registered';
+        message.style.color = 'green';
+        messageContainer.style.borderColor = 'green';
+    }
 }
+
+function storeFormData(){
+    const user={
+        name:form.name.value,
+        phone:form.phone.value,
+        email:form.email.value,
+        website:form.website.value,
+        password:form.password.value
+    };
+
+    console.log('user->',user);
+}
+
 
 
 function processFormData(e){
     e.preventDefault();
     // Validate form
     validateForm();
+    // submit form data if valid
+    if(isValid && passwordsMatch){
+        storeFormData();
+    }
+
 }
 
 
